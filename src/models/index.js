aebb46b76376622a4a1df3d2eacc197ae249ae34
Dario Vitorte <vitortedario@gmail.com>
2022-01-20 15:46:24 -0800

    datacollection finished

--- src/models/index.js
+++ src/models/index.js
@@ -5,4 +5,26 @@ const { Sequelize, DataTypes } = require('sequelize');
 // Models
 const QuoteModel = require('./quote.js');
 const UserModel = require('./user.js');
+const Collection = require('./data-collection.js');
 
+const DATABASE_URL = process.env.NODE_ENV === 'test' ?
+ 'sqlite:memory' :
+  process.env.DATABASE_URL;
+
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ?
+ {
+   dialecticOptions: {
+     ssl: true,
+     rejectUnauthorized: false,
+   }
+ } : {};
+
+ const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+ const Quotes = QuoteModel(sequelize, DataTypes);
+ const Users = UserModel(sequelize, DataTypes);
+
+ module.exports = {
+   db: sequelize,
+   Quotes: new Collection(Quotes),
+   Users: new Collection(Users),
+ }

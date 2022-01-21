aa2ea7eba8de5632d89e17d180d3a64e72201c63
Dario Vitorte <vitortedario@gmail.com>
2022-01-20 17:32:20 -0800

    quickfix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -37,7 +37,7 @@ router.get('/users', bearerAuth, permissions('delete'), async (req, res, next) =
   res.status(200).json(usersList);
 });
 
-router.update('/users/:id', bearerAuth, permissions('update'), async (req, res, next) => {
+router.put('/users/:id', bearerAuth, permissions('update'), async (req, res, next) => {
   const updatedUser = await Users.update(req.params.id, req.body);
   res.status(200).json(updatedUser.username);
 });

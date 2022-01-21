6477bb4943d995050f862c95313bb8a677bc367b
Dario Vitorte <vitortedario@gmail.com>
2022-01-20 17:04:19 -0800

    routes finished

---
+++ src/routes/quote.routes.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+
+const { Quotes } = require('../models');
+
+const bearerAuth = require('../middleware/bearer');
+
+const permissions = require('../middleware/acl');
+
+const router = express.Router();
+
+router.get('/quotes', bearerAuth, handleGetAll);
+
+router.get('/quotes/:id', bearerAuth, handleGetOne);
+
+router.put('/quotes/:id', bearerAuth, permissions('update'), handleUpdate);
+
+router.post('/quotes', bearerAuth, permissions('create'), handleCreate);
+
+router.delete('/quotes/:id', bearerAuth, permissions('delete'), handleDelete);
+
+async function handleGetAll(req, res) {
+  const allQuotes = await Quotes.get();
+  res.status(200).json(allQuotes);
+}
+
+async function handleGetOne(req, res) {
+  const id = req.params.id;
+  const oneQuote = await Quotes.get(id);
+  res.status(200).json(oneQuote);
+}
+
+async function handleUpdate(req, res) {
+  const id = req.params.id;
+  const obj = req.body;
+  const updatedQuote = await Quotes.update(id, obj);
+  res.status(200).json(updatedQuote);
+}
+
+async function handleCreate(req, res) {
+  const obj = req.body;
+  const newQuote = await Quotes.create(obj);
+  res.status(200).json(newQuote);
+}
+
+async function handleDelete(req, res) {
+  const id = req.params.id;
+  const deletedQuote = await Quotes.delete(id);
+  res.status(200).json(deletedQuote);
+}
+
+module.exports = router;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+const { Users } = require('../models/index');
+
+const basicAuth = require('../middleware/basic');
+
+const bearerAuth = require('../middleware/bearer');
+
+const permissions = require('../middleware/acl');
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const userRecord = await Users.create(req.body);
+    const output = {
+      user: userRecord,
+      token: userRecord.token
+    };
+    res.status(201).json(output);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+router.post('/signin', basicAuth, (req, res, next) => {
+  const user = {
+    user: req.userInfo,
+    token: req.userInfo.token
+  };
+  res.status(200).json(user);
+});
+
+router.get('/users', bearerAuth, permissions('delete'), async (req, res, next) => {
+  const userRecords = await Users.get();
+  const usersList = userRecords.map(user => user.username);
+  res.status(200).json(usersList);
+});
+
+router.update('/users/:id', bearerAuth, permissions('update'), async (req, res, next) => {
+  const updatedUser = await Users.update(req.params.id, req.body);
+  res.status(200).json(updatedUser.username);
+});
+
+router.delete('/users/:id', bearerAuth, permissions('delete'), async (req, res, next) => {
+  const userToDelete = await Users.delete(req.params.id);
+  res.status(204).send(userToDelete); //TODO: check if this returns empty object or null/undefined
+});
+
+module.exports = router;
